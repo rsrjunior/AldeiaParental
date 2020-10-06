60d63e1cbe1e0cba6b48c3377e38f1dba1ed96f1
Roberto Rosa Junior <beto.junior@gmail.com>
2020-10-06 19:05:09 +0100

    Custom Register. UC 1.1

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -22,16 +22,21 @@ namespace AldeiaParental.Areas.Identity.Pages.Account
     {
         private readonly SignInManager<AldeiaParentalUser> _signInManager;
         private readonly UserManager<AldeiaParentalUser> _userManager;
+        private readonly RoleManager<AldeiaParentalRole> _roleManager;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
 
+        private const string _customerRole = "Cliente";
+        private const string _caregiverRole = "Cuidador";
         public RegisterModel(
             UserManager<AldeiaParentalUser> userManager,
+            RoleManager<AldeiaParentalRole> roleManager,
             SignInManager<AldeiaParentalUser> signInManager,
             ILogger<RegisterModel> logger,
             IEmailSender emailSender)
         {
             _userManager = userManager;
+            _roleManager = roleManager;
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
@@ -46,6 +51,21 @@ namespace AldeiaParental.Areas.Identity.Pages.Account
 
         public class InputModel
         {
+            [Required]
+            [StringLength(30, ErrorMessage = "The {0} must have max {1} characters long.")]
+            [Display(Name = "First name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [StringLength(70, ErrorMessage = "The {0} must have max {1} characters long.")]
+            [Display(Name = "Last name")]
+            public string LastName { get; set; }
+
+            [Display(Name = "Customer")]
+            public bool Customer { get; set; }
+            [Display(Name = "Caregiver")]
+            public bool Caregiver { get; set; }
+
             [Required]
             [EmailAddress]
             [Display(Name = "Email")]
@@ -68,18 +88,71 @@ namespace AldeiaParental.Areas.Identity.Pages.Account
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
-
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
             returnUrl = returnUrl ?? Url.Content("~/");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new AldeiaParentalUser { UserName = Input.Email, Email = Input.Email };
+                var user = new AldeiaParentalUser 
+                { UserName = Input.Email,
+                    Email = Input.Email, 
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName };
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+
+                    //try to set roles
+
+                    #region set roles
+                    if (Input.Customer)
+                    {
+
+                        bool customerRoleFound = await _roleManager.RoleExistsAsync(_customerRole);
+                        if (!customerRoleFound) {
+                            _logger.LogWarning("Could not set role for " + user.UserName
+                            + " The role " + _customerRole + "does not exist");
+                        }
+                        else
+                        {
+                            try
+                            {
+                                await _userManager.AddToRoleAsync(user, _customerRole);
+                            }
+                            catch (Exception ex)
+                            {
+                                _logger.LogWarning("Could not set "+ _customerRole + " role for " + user.UserName
+                            + " "+ex.Message);
+                            }
+                        }
+                    }
+
+                    if (Input.Caregiver)
+                    {
+
+                        bool caregiverRoleFound = await _roleManager.RoleExistsAsync(_caregiverRole);
+                        if (!caregiverRoleFound)
+                        {
+                            _logger.LogWarning("Could not set role for " + user.UserName
+                            + " The role " + _caregiverRole + "does not exist");
+                        }
+                        else
+                        {
+                            try
+                            {
+                                await _userManager.AddToRoleAsync(user, _caregiverRole);
+                            }
+                            catch (Exception ex)
+                            {
+                                _logger.LogWarning("Could not set " + _caregiverRole + " role for " + user.UserName
+                            + " " + ex.Message);
+                            }
+                        }
+                    }
+                    #endregion
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -2,23 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AldeiaParental.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace AldeiaParental.Pages
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly UserManager<AldeiaParentalUser> _userManager;
+        private readonly SignInManager<AldeiaParentalUser> _signInManager;
 
-        public IndexModel(ILogger<IndexModel> logger)
+
+
+        public IndexModel(ILogger<IndexModel> logger,
+            UserManager<AldeiaParentalUser> userManager,
+            SignInManager<AldeiaParentalUser> signInManager)
         {
-            _logger = logger;
+            _logger = logger; 
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
-        public void OnGet()
+        public string UserFirstName { get; set; }
+        public IList<string> UserRoles { get; set; } 
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        private async Task LoadAsync(AldeiaParentalUser user)
         {
+            UserFirstName = user.FirstName;
+            UserRoles = await _userManager.GetRolesAsync(user);
+
         }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+            UserFirstName = user.FirstName;
+            UserRoles = await _userManager.GetRolesAsync(user);
+
+            //await LoadAsync(user);
+            return Page();
+        }
+
+
+
     }
 }
