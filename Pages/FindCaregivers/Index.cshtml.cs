2b757c5e6836fa65ce9725952631c14e7b49af56
Roberto Rosa Junior <beto.junior@gmail.com>
2020-10-12 22:12:25 +0100

    async Warning message clear

--- Pages/FindCaregivers/Index.cshtml.cs
+++ Pages/FindCaregivers/Index.cshtml.cs
@@ -58,7 +58,7 @@ namespace AldeiaParental.Pages.FindCaregivers
                 FirstName = s.User.FirstName
             }).Distinct();
 
-            Caregivers = userList.ToList();
+            Caregivers = await userList.ToListAsync();
          
 
             return Page();
