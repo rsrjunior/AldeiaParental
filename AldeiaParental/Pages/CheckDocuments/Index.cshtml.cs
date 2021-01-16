5b3cb5f77f2fb73c22ac18990097c28d925996d5
Roberto Rosa Junior <beto.junior@gmail.com>
2021-01-16 20:01:36 +0000

    documentos e regioes - csv

--- AldeiaParental/Pages/CheckDocuments/Index.cshtml.cs
+++ AldeiaParental/Pages/CheckDocuments/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using AldeiaParental.Data;
 using AldeiaParental.Models;
+using System.Text;
 
 namespace AldeiaParental.Pages_CheckDocuments
 {
@@ -25,6 +26,20 @@ namespace AldeiaParental.Pages_CheckDocuments
         {
             PersonalDocument = await _context.PersonalDocument
                 .Include(p => p.User).ToListAsync();
+        }
+        public async Task<IActionResult> OnGetCsv()
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine("Tipo,Número,Verificação,Usuário");
+            List<PersonalDocument> _documents = await _context.PersonalDocument.ToListAsync<PersonalDocument>();
+            foreach (var doc in _documents)
+            {
+                builder.Append($"{doc.DocumentType},{doc.DocumentNumber},");
+                builder.Append($"{(doc.Valid==null?"Indefinido":doc.Valid==true?"Válido":"Inválido")},");
+                builder.AppendLine($"{doc.UserId}");
+            }
+
+            return File(Encoding.UTF8.GetBytes(builder.ToString()), "text/csv", "userdocuments.csv");
         }
     }
 }
--- AldeiaParental/Pages/Regions/Index.cshtml.cs
+++ AldeiaParental/Pages/Regions/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using AldeiaParental.Data;
 using AldeiaParental.Models;
+using System.Text;
 
 namespace AldeiaParental.Pages.Regions
 {
@@ -24,6 +25,18 @@ namespace AldeiaParental.Pages.Regions
         public async Task OnGetAsync()
         {
             Region = await _context.Region.ToListAsync();
+        }
+        public async Task<IActionResult> OnGetCsv()
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine("Nome,Descrição");
+            List<Region> _regions = await _context.Region.ToListAsync<Region>();
+            foreach (var region in _regions)
+            {
+                builder.AppendLine($"{region.Name},{region.Description}");
+            }
+
+            return File(Encoding.UTF8.GetBytes(builder.ToString()), "text/csv", "regions.csv");
         }
     }
 }
