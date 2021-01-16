aac5b88179ebb09f298db82771905963ff4b8e81
Roberto Rosa Junior <beto.junior@gmail.com>
2021-01-16 19:21:20 +0000

    Ajustes Views Users

--- AldeiaParental/Pages/Users/Index.cshtml.cs
+++ AldeiaParental/Pages/Users/Index.cshtml.cs
@@ -8,33 +8,73 @@ using Microsoft.EntityFrameworkCore;
 using AldeiaParental.Data;
 using AldeiaParental.Models;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace AldeiaParental.Pages_Users
 {
     public class IndexModel : PageModel
     {
         private readonly AldeiaParental.Data.ApplicationDbContext _context;
+        private readonly UserManager<AldeiaParentalUser> _userManager;
 
-        public IndexModel(AldeiaParental.Data.ApplicationDbContext context)
+        public IndexModel(AldeiaParental.Data.ApplicationDbContext context,
+        UserManager<AldeiaParentalUser> roleManager)
         {
             _context = context;
+            _userManager = roleManager;
         }
 
-        public IList<AldeiaParentalUser> AldeiaParentalUser { get;set; }
+        public IList<UserViewModel> Users { get;set; }
 
         public async Task OnGetAsync()
         {
-            AldeiaParentalUser = await _context.Users.ToListAsync();
+            List<AldeiaParentalUser> _users = await _context.Users.ToListAsync();
+            Users = new List<UserViewModel>();
+            foreach (var user in _users)
+            {
+                Users.Add(new UserViewModel
+                        {
+                            Id = user.Id,
+                            FirstName = user.FirstName,
+                            LastName = user.LastName,
+                            Email = user.Email,
+                            LockoutEnd = user.LockoutEnd,
+                            Perfis = await _userManager.GetRolesAsync(user),
+                            RegistrationDate = user.RegistrationDate                           
+                        });
+            }
         }
         
-        public IActionResult OnGetCsv()
+        public class UserViewModel
+        {
+            //Nome	Sobrenome	Email	Perfil	RegistrationDate	LockoutEnd
+            public string Id { get; set; }
+            [Display(Name = "Nome")]
+            public string FirstName { get; set; }
+            [Display(Name = "Sobrenome")]
+            public string LastName { get; set; }
+            public string Email { get; set; }
+            public IList<string> Perfis { get; set; }
+            [Display(Name = "Cadastro")]
+            public DateTime? RegistrationDate { get; set; }
+            [Display(Name = "Fim Bloqueio")]
+            public DateTimeOffset? LockoutEnd { get; set; }
+        }
+        public async Task<IActionResult> OnGetCsv()
         {
             var builder = new StringBuilder();
-            builder.AppendLine("Id,Username,Email");
+            builder.AppendLine("Id,Nome,Sobrenome,Email,Perfis,Cadastro,FimBloqueio");
             List<AldeiaParentalUser> _users = _context.Users.ToList<AldeiaParentalUser>();
             foreach (var user in _users)
             {
-                builder.AppendLine($"{user.Id},{user.FirstName},{user.Email}");
+                builder.Append($"{user.Id},{user.FirstName},{user.LastName},{user.Email},");
+                IList<string> roles = await _userManager.GetRolesAsync(user);
+                foreach (string item in roles)
+                {
+                    builder.Append($"{item}");
+                }
+                builder.AppendLine($",{user.RegistrationDate},{user.LockoutEnd}");
             }
 
             return File(Encoding.UTF8.GetBytes(builder.ToString()), "text/csv", "users.csv");
