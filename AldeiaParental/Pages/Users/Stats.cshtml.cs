f6145fdb969b2f99003b0aaf4b8dbfab23da1715
Roberto Rosa Junior <beto.junior@gmail.com>
2021-01-18 16:41:43 +0000

    grafico usuarios por mes US 3.3

--- AldeiaParental/Pages/Users/Stats.cshtml.cs
+++ AldeiaParental/Pages/Users/Stats.cshtml.cs
@@ -10,7 +10,8 @@ public class StatsModel : PageModel
 {
     private readonly AldeiaParental.Data.ApplicationDbContext _context;
     private readonly UserManager<AldeiaParentalUser> _userManager;
-    public string ChartJson { get; set; }
+    public string UsersProfileChartJson { get; set; }
+    public string MonthUsersChartJson { get; set; }
 
 
     public StatsModel(AldeiaParental.Data.ApplicationDbContext context,
@@ -20,9 +21,15 @@ public class StatsModel : PageModel
         _userManager = userManager;
     }
 
+    private int getMonthIndex(int increment) => System.DateTime.Now.AddMonths(increment).Month;
+    
+    private string getMonthLabel(int increment) => 
+        System.DateTime.Now.AddMonths(increment).ToString("MMM-yy", System.Globalization.CultureInfo.CreateSpecificCulture("pt-BR"));
+
     public async Task OnGetAsync()
     {
         int cliente = 0, cuidador = 0, ambos = 0;
+        int[] monthRegister = new int[13];
 
         List<AldeiaParentalUser> _users = await _context.Users.ToListAsync();
         foreach (var user in _users)
@@ -34,6 +41,9 @@ public class StatsModel : PageModel
                 cliente++;
             else if (roles.Contains("Cuidador"))
                 cuidador++;
+
+            if(user.RegistrationDate.HasValue && user.RegistrationDate > (System.DateTime.Now.AddYears(-1)))
+                monthRegister[user.RegistrationDate.Value.Month]++;
         }
 
 
@@ -46,7 +56,7 @@ public class StatsModel : PageModel
             {
                 labels: ['Cliente', 'Cuidador', 'Ambos'],
                 datasets: [{
-                    label: '# of Votes',");
+                    label: '# usuários',");
         builder.Append($"data: [{cliente}, {cuidador}, {ambos}],");
         builder.Append(@"backgroundColor: [
                     'rgba(255, 99, 132, 0.5)',
@@ -70,8 +80,33 @@ public class StatsModel : PageModel
         }");
 
         // Ref: https://www.chartjs.org/docs/latest/
-        ChartJson = builder.ToString();
+        UsersProfileChartJson = builder.ToString();
 
+        builder.Clear();
+        builder.Append("{type: 'line',responsive: true,data:{");
+
+        builder.Append($"labels: ['{getMonthLabel(-11)}', '{getMonthLabel(-10)}', '{getMonthLabel(-9)}', '{getMonthLabel(-8)}',  ");
+        builder.Append($"'{getMonthLabel(-7)}', '{getMonthLabel(-6)}', '{getMonthLabel(-5)}', '{getMonthLabel(-4)}',  ");
+        builder.Append($"'{getMonthLabel(-3)}', '{getMonthLabel(-2)}', '{getMonthLabel(-1)}', '{getMonthLabel(0)}'],");
+        builder.Append("datasets: [{label: 'novos cadastros',");
+        builder.Append($"data: [{monthRegister[getMonthIndex(-11)]}, {monthRegister[getMonthIndex(-10)]}, {monthRegister[getMonthIndex(-9)]},");
+        builder.Append($"{monthRegister[getMonthIndex(-8)]}, {monthRegister[getMonthIndex(-7)]}, {monthRegister[getMonthIndex(-6)]},");
+        builder.Append($"{monthRegister[getMonthIndex(-5)]}, {monthRegister[getMonthIndex(-4)]}, {monthRegister[getMonthIndex(-3)]},");
+        builder.Append($"{monthRegister[getMonthIndex(-2)]}, {monthRegister[getMonthIndex(-1)]}, {monthRegister[getMonthIndex(0)]}],");
+        builder.Append(@"backgroundColor: ['rgba(54, 162, 235, 0.5)'],
+                    borderColor: ['rgba(54, 162, 235, 1)'],
+                    borderWidth: 1
+                    }]
+            },
+             options: {
+                 title: {
+                     display: true,
+                     text: 'Cadastro mensal'
+                     }
+                }
+        }");
+
+        MonthUsersChartJson = builder.ToString();
         // Chart = JsonConvert.DeserializeObject<ChartJs>(chartData);
         // ChartJson = JsonConvert.SerializeObject(Chart, new JsonSerializerSettings
         // {
