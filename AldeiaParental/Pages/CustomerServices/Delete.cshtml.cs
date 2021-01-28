b3a00a4cb1cf77f5e745db9f4df7dc4ae8a24b85
Roberto Rosa Junior <beto.junior@gmail.com>
2021-01-28 19:01:51 +0000

    excluir agendamento

---
+++ AldeiaParental/Pages/CustomerServices/Delete.cshtml.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AldeiaParental.Data;
+using AldeiaParental.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace AldeiaParental.Pages_CustomerServices
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly AldeiaParental.Data.ApplicationDbContext _context;
+        private readonly UserManager<AldeiaParentalUser> _userManager;
+
+        public DeleteModel(AldeiaParental.Data.ApplicationDbContext context,
+            UserManager<AldeiaParentalUser> userManager)
+        {
+            _context = context;        
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public Service Service { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Service = await _context.Service
+                .Where(s=>s.CustomerId == _userManager.GetUserId(User))
+                .Include(s => s.Caregiver)
+                .Include(s => s.Customer).FirstOrDefaultAsync(s => s.Id == id);
+
+            if (Service == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Service = await _context.Service.FindAsync(id);
+
+            if (Service != null)
+            {
+                if (Service.CustomerId ==_userManager.GetUserId(User)&&
+                    Service.datetime >= DateTime.Now)
+                {
+                    _context.Service.Remove(Service);
+                    await _context.SaveChangesAsync();
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
